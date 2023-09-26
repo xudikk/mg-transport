34949184f73394e9122ca8aa54ae5fd2e07852b1
xudikk <xudikk.1@gmail.com>
2023-09-26 11:19:46 +0500

    settings.py

--- src/settings.py
+++ src/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# if DEBUG:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.'+os.getenv('DB_ENGINE', "postgresql"),
-#             'NAME': os.getenv("POSTGRES_DB"),
-#             'USER': os.getenv("POSTGRES_USER"),
-#             'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-#             'HOST': os.getenv('POSTGRES_HOST'),
-#             'PORT': os.getenv("POSTGRES_PORT"),
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.'+os.getenv('DB_ENGINE', "postgresql"),
+            'NAME': os.getenv("POSTGRES_DB"),
+            'USER': os.getenv("POSTGRES_USER"),
+            'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+            'HOST': os.getenv('POSTGRES_HOST'),
+            'PORT': os.getenv("POSTGRES_PORT"),
+        }
+    }
 
 #Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
